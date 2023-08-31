aec308716e8f675effa2916b65aaee35f8e060ba
James Leong <jyleong@users.noreply.github.com>
2023-08-30 17:25:21 -0700

    commit custom bot on relationship documents

--- ingest.py
+++ ingest.py
@@ -1,21 +1,32 @@
 """Load html from files, clean up, split, ingest into Weaviate."""
 import pickle
 
-from langchain.document_loaders import ReadTheDocsLoader
+from langchain.document_loaders import UnstructuredEPubLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 
+BOOKS = [
+    "documents/Malcolm Collins - The Pragmatist's Guide to Relationships-Pragmatist Foundation Inc., The (2020).epub",
+    "documents/Malcolm Collins_ Simone Collins - The Pragmatist's Guide to Sexuality-Pragmatist Foundation Inc., The (2020).epub",
+    "documents/Nonviolent Communication_ A Language of Life -- Marshall B. Rosenberg [Rosenberg, Marshall B.] -- 5a7aff2c7cad8595c7a81aeb7924c03e -- Anna’s Archive.epub",
+    "documents/orca_share_media1477764558633.epub",
+    "documents/The Relationship Cure - A 5 Step Guide to Strengthening Your Marriage, Family, and Friendships -- John Gottman, Joan DeClaire -- 2017;0 -- Potter_TenSpeed_Harmony -- 9780609809532 -- 599ff3c2721dd4c6268e627e1404123c -- Anna’s Archive.epub",
+    "documents/The Seven Principles for Making Marriage Work_ A Practical Guide from the Country’s Foremost -- John Gottman Ph.D., Nan Silver -- Revised ed., 2015 -- Harmony -- 9782014034165 -- 36c8e06afc0c18ec9f63ed175ecf2260 -- Anna’s Archive.epub"
+]
 
 def ingest_docs():
     """Get documents from web pages."""
-    loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
-    raw_documents = loader.load()
+    documents = []
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
     )
-    documents = text_splitter.split_documents(raw_documents)
+    for book in BOOKS:
+        epub_loader = UnstructuredEPubLoader(book, mode="elements")
+        docs = epub_loader.load_and_split(text_splitter)
+        documents += docs
+    
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.from_documents(documents, embeddings)
 
--- query_data.py
+++ query_data.py
@@ -1,18 +1,40 @@
 """Create a ChatVectorDBChain for question/answering."""
-from langchain.callbacks.base import AsyncCallbackManager
+from langchain.callbacks.manager import AsyncCallbackManager
 from langchain.callbacks.tracers import LangChainTracer
-from langchain.chains import ChatVectorDBChain
+from langchain.chains import ConversationalRetrievalChain
 from langchain.chains.chat_vector_db.prompts import (CONDENSE_QUESTION_PROMPT,
                                                      QA_PROMPT)
 from langchain.chains.llm import LLMChain
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
+from langchain.memory import ConversationSummaryBufferMemory, ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 from langchain.vectorstores.base import VectorStore
 
+RELATIONSHIP_COUNCILLOR_PROMPT = PromptTemplate.from_template(
+"""
+Chat History:
+{chat_history}
+You are a relationship and communication coach referencing.
+You are given questions or prompts on their communication giving suggestions on how the conversation is going.
+
+Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+{context}
+
+If your answer is sourced from relationship resource,
+politely inform them and provide the document source.
+
+If the question is not about dating, relationships or why a conversation is not going well,
+tell them that you are tuned for questions and prompts around dating, relationships and conversations.
+
+Question: {question}
+Helpful, friendly and thoughtful Answer:
+    """
+)
 
 def get_chain(
     vectorstore: VectorStore, question_handler, stream_handler, tracing: bool = False
-) -> ChatVectorDBChain:
+) -> ConversationalRetrievalChain:
     """Create a ChatVectorDBChain for question/answering."""
     # Construct a ChatVectorDBChain with a streaming llm for combine docs
     # and a separate, non-streaming llm for question generation
@@ -39,16 +61,22 @@ def get_chain(
     )
 
     question_generator = LLMChain(
-        llm=question_gen_llm, prompt=CONDENSE_QUESTION_PROMPT, callback_manager=manager
+        llm=question_gen_llm,
+        prompt=CONDENSE_QUESTION_PROMPT,
+        callback_manager=manager
     )
     doc_chain = load_qa_chain(
-        streaming_llm, chain_type="stuff", prompt=QA_PROMPT, callback_manager=manager
+        streaming_llm,
+        chain_type="stuff",
+        prompt=RELATIONSHIP_COUNCILLOR_PROMPT,
+        callback_manager=manager
     )
 
-    qa = ChatVectorDBChain(
-        vectorstore=vectorstore,
+    qa = ConversationalRetrievalChain(
+        retriever=vectorstore.as_retriever(),
         combine_docs_chain=doc_chain,
         question_generator=question_generator,
-        callback_manager=manager,
+        memory=ConversationSummaryBufferMemory(llm=streaming_llm, memory_key='chat_history', return_messages=True, output_key='answer'),
+        verbose=True
     )
     return qa
